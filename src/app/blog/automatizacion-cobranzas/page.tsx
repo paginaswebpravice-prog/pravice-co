c7b87ea1da90e15904bbf5fa83a2e32bbc7cf776
Jeffer-Andres-Tafurth-Cortes <jeffer.andres.tafurth.cortes@gmail.com>
2026-03-12 09:47:29 -0500

    Updates styles

--- src/app/blog/automatizacion-cobranzas/page.tsx
+++ src/app/blog/automatizacion-cobranzas/page.tsx
@@ -1,8 +1,6 @@
 "use client";
 
 import { motion } from "framer-motion";
-import Image from "next/image";
-import Link from "next/link";
 import styles from "./Article.module.css";
 
 const fadeUp = {
