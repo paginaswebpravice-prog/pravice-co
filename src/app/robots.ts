26a01541d6db6cd02fd2741493701c4caa933472
Jeffer-Andres-Tafurth-Cortes <jeffer.andres.tafurth.cortes@gmail.com>
2026-03-31 10:44:06 -0500

    Updates sitemap

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: "*",
       allow: "/",
     },
-    sitemap: "https://pravice.co/sitemap.xml",
+    sitemap: "https://www.pravice.co/sitemap.xml",
   };
 }
