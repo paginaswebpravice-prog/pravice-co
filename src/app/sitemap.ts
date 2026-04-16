1e1f3008d9f346480c56f6b2dba0da06ca57fad3
Jeffer-Andres-Tafurth-Cortes <jeffer.andres.tafurth.cortes@gmail.com>
2026-04-16 17:55:28 -0500

    Updates sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -30,8 +30,8 @@ export default function sitemap(): MetadataRoute.Sitemap {
   const blogUrls = blogArticles.map((slug) => ({
     url: `${baseUrl}/blog/${slug}`,
     lastModified: today,
-    changeFrequency: "monthly" as const,
-    priority: 0.7,
+    changeFrequency: "weekly" as const,
+    priority: 0.8,
   }));
 
   return [
