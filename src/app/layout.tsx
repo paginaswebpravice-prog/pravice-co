59d06b68b42a3ce0ad7e7f90e9caae4f7934ea40
Jeffer-Andres-Tafurth-Cortes <jeffer.andres.tafurth.cortes@gmail.com>
2026-05-11 16:03:16 -0500

    Add event google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -130,10 +130,14 @@ export default function RootLayout({
         <Script id="google-analytics" strategy="afterInteractive">
           {`
             window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-MNESPQQ54T');
-          `}
+
+            window.gtag = function(){
+            window.dataLayer.push(arguments);
+            };
+
+            window.gtag('js', new Date());
+            window.gtag('config', 'G-MNESPQQ54T');
+        `}
         </Script>
 
         {/* Schema */}
